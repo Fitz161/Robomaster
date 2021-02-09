6793edcf03903fdd4614a7a1c706594cfc131036
Fitz161 <Fitz161@users.noreply.github.com>
2021-02-09 09:48:00 +0800

    识别待击打装甲板
    对扇叶和装甲板去重，根据扇叶和装甲板距离关系筛选待击打装甲板

--- windmill/detect.py
+++ windmill/detect.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
-PATH = r'D:/photos/robomaster/2.jpg'
+PATH = r''
 img = cv2.imread(PATH, 1)
 gray_img = cv2.imread(PATH, 0)
 blue, green, red = cv2.split(img)
 
 print('image shape:', img.shape)
-red_img = red - blue # 采用红色通道与蓝色通道的差作为待处理图像
+red_img = red - blue  # 采用红色通道与蓝色通道的差作为待处理图像
 
 # 去除背景
 width, height = red_img.shape
@@ -23,9 +23,9 @@ bin_img = cv2.threshold(red_img, 200, 255, cv2.THRESH_BINARY)[1]
 bin_img = np.where(bin_img > 180, 0, 255)
 
 # 对其中白色部分先膨胀，再腐蚀，去除内部噪声
-kernel = np.ones((4, 4), np.uint8)  #核越大则图形越膨胀变粗，内部噪点也越少，需避免相交
+kernel = np.ones((4, 4), np.uint8)  # 核越大则图形越膨胀变粗，内部噪点也越少，需避免相交
 
-bin_img = bin_img.astype(np.uint8) #int32转成uint8
+bin_img = bin_img.astype(np.uint8)  # int32转成uint8
 dilation = cv2.dilate(bin_img, kernel, iterations=2)
 erosion = cv2.erode(dilation, kernel, iterations=2)
 
@@ -43,42 +43,111 @@ edge = cv2.Canny(erosion, 0, 255)  # 后两参数无影响
 contours, hierarchy = cv2.findContours(
     edge, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# 分离扇叶和装甲板轮廓
+blade_width = []  # 用于求扇叶平均宽度，作为下方合并相邻扇叶的阈值
+
+# 分离扇叶和装甲板轮廓和对应中心点
 armor_list, armor_center = [], []
 blade_list, blade_center = [], []
 for contour in contours:
-    #返回轮廓最小外接矩形的 中心(x,y), (宽,高), 旋转角度
+    # 返回轮廓最小外接矩形的 中心(x,y), (宽,高), 旋转角度
     rectangle = cv2.minAreaRect(contour)
-    armor_center.append(rectangle[0])
-    length = sorted(rectangle[1])  #宽和高的大小不确定
-    #print(length[1] / length[0])   # 1.6  3.8
+    length = sorted(rectangle[1])  # 宽和高的大小不确定
+    # print(length[1] / length[0])   # 1.6  3.8
     if length[1] / length[0] > 2:
+        blade_width.append(length[0])
         blade_list.append(cv2.boxPoints(rectangle))
+        blade_center.append(rectangle[0])
     else:
         # 获取该矩形的四个顶点坐标
         armor_list.append(cv2.boxPoints(rectangle))
+        armor_center.append(rectangle[0])
+
 
 # 通过画线方式来在原图像中绘制矩形
-for box in armor_list:
+for armor in armor_list:
     for j in range(4):
-        cv2.line(img, tuple(box[j]), tuple(
-            box[(j + 1) % 4]), (255, 0, 0), 2)
+        cv2.line(img, tuple(armor[j]), tuple(
+            armor[(j + 1) % 4]), (255, 0, 0), 2)
 
-# 对矩形中心去重
-filtered_center = []
+# 对矩形装甲板中心去重
+filtered_armor_center = []
 while armor_center:
     center = armor_center.pop()
     for point in armor_center:
-        if abs(center[0] - point[0]) > 10 or abs(center[1] - point[1]) > 10:
+        if abs(center[0] - point[0]) < 5 and abs(center[1] - point[1]) < 5:
             armor_center.remove(point)
-    filtered_center.append(center)
+    filtered_armor_center.append(center)
 
-print(filtered_center)
+# 对扇叶中心去重
+filtered_blade_center = []
+while blade_center:
+    center = blade_center.pop()
+    for point in blade_center:
+        if abs(center[0] - point[0]) < 10 and abs(center[1] - point[1]) < 10:
+            blade_center.remove(point)
+    filtered_blade_center.append(center)
 
-# 中值滤波,去除噪音
-# blured_img = cv2.medianBlur(bin_img, ksize=5)  # 只能取奇数, 越大图像噪点越少
-#
-# print(cv2.HoughCircles(blured_img, cv2.HOUGH_GRADIENT, dp=2, minDist=100))#, minRadius=5, maxRadius=10))
+
+width = int(sum(blade_width) / len(blade_width)) if blade_width else None
+
+# 获取相邻两片扇叶的中心, 并放到blade_center中
+while filtered_blade_center:
+    center = filtered_blade_center.pop()
+    for point in filtered_blade_center:
+        # 用二倍扇叶长度做阈值
+        if abs(center[0] - point[0]) < width * \
+                2 and abs(center[1] - point[1]) < width * 2:
+            blade_center.append(
+                ((center[0] + point[0]) / 2,
+                 (center[1] + point[1]) / 2))
+            filtered_blade_center.remove(point)
+            break
+
+print('blade_center', blade_center)
+print('filtered_armor_center', filtered_armor_center)
+
+filtered_armor_center = [tuple(map(int, num_tuple))
+                         for num_tuple in filtered_armor_center]  # 浮点数转整数
+
+
+blade_center_num = len(blade_center)
+
+
+def distance(point1, point2):
+    """
+    return the distance of two points
+    :param point1: (x, y) or [x, y] representing coordinates of the point
+    :param point2: (x, y) or [x, y]
+    :return: float number
+    """
+    return ((point1[0] - point2[0]) ** 2 +
+            (point1[1] - point2[1]) ** 2) ** (1 / 2)
+
+
+if not blade_center_num:  # 无扇叶时，直接将唯一的装甲板识别为待打击对象
+    cv2.circle(
+        img,
+        filtered_armor_center[0],
+        4,
+        (255, 255, 0),
+        thickness=-1)  # thickness为负表示填充圆形
+else:  # 有扇叶时，已知相邻两扇叶的中心，计算与每个装甲板中心的距离，最近的即为配对的装甲板，筛选剩的即为待击打对象
+    for blade_point in blade_center:
+        distance_list = []
+        for armor_point in filtered_armor_center:
+            distance_list.append(distance(blade_point, armor_point))
+        # 找到离扇叶中心最近的装甲板并将其删除
+        index = distance_list.index(min(distance_list))
+        del filtered_armor_center[index]
+    # 画出最后剩下的一个装甲板的位置
+    if filtered_armor_center:
+        cv2.circle(
+            img,
+            filtered_armor_center[0],
+            4,
+            (255, 255, 0),
+            thickness=-1)  # thickness为负表示填充圆形
+
 
 cv2.imshow('', img)
 cv2.waitKey(0)
